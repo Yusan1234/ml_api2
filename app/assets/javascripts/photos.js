3f1db1f0ce9219771c667d05fa62f3690e0c607d
Yusan1234 <yxa805terms@gmail.com>
2020-08-08 13:36:38 +0900

    connect with docker and return image

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,13 +1,36 @@
 $(function(){
     var elements = $('.modal-overlay, .modal');
-    
-    $('#imgclick').click(function(){
-        var id = $(this).val();
-        alert(id);
+    $('.img_01_show').click(function(){
+    // $('#imgclick').click(function(){
+        // var id = $(this).val();
+        // alert(id);
         elements.addClass('active');
     });
     
     $('.close-modal').click(function(){
         elements.removeClass('active');
     });
+    var bar=$('#progress_bar');
+    var percentage=parseInt($('#progress_percentage').html());
+
+    function stopProgress(){
+    clearInterval(progress);
+    }
+
+    var progress= setInterval(function(){
+    percentage++;
+    if (percentage<=100){
+        $('#progress_percentage').html(percentage+'%');
+        if (percentage>10) {
+        bar.css('width',percentage+'%');
+        console.log(percentage);
+        }
+    }
+    else {
+        stopProgress()
+    }
+    },80);
+
     });
+
+    
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,26 +4,44 @@ class PhotosController < ApplicationController
   require 'json'
   require 'rubygems'
   require 'RMagick'
+  require 'timeout'
+
   def new
     @photo = Photo.new
     @DevImg = Photo.where(name: 'dev')
     gon.img = @DevImg
   end
-  def index
-    image_file = File.open("public/uploads/photo/image/dancing.jpg")
-    styled_file = File.open("public/uploads/photo/image/dancing.jpg")
-    base64_image = Base64.strict_encode64(image_file.read)
-    base64_image_style = Base64.strict_encode64(styled_file.read)
-    data = [
-      ["content_img", base64_image],
-      ['style_img',  base64_image_style]
-    ].to_json
-    # req.set_form(data, "multipart/form-data")
 
-    uri = URI.parse("http://0.0.0.0:8888/predict")
-    req = Net::HTTP.new(uri.host, uri.port)
-    req.post(uri.path, data)
+  def index
+  begin
+    Timeout.timeout(30000) do
+      image_file = File.open("public/uploads/photo/image/dancing.jpg")
+      styled_file = File.open("public/uploads/photo/image/dancing.jpg")
+      base64_image = Base64.strict_encode64(image_file.read)
+      base64_image_style = Base64.strict_encode64(styled_file.read)
+      uri = URI.parse("http://0.0.0.0:8888/predict")
+      https = Net::HTTP.new(uri.host, uri.port)
+       
+      # https.use_ssl = true # HTTPSでよろしく
+      req = Net::HTTP::Post.new(uri.request_uri)
+       
+      req["Content-Type"] = "application/json" # httpリクエストヘッダの追加
+      payload = {
+        "content_img" => base64_image,
+           "style_img" => base64_image_style
+      }.to_json
+      req.body = payload # リクエストボデーにJSONをセット
+      res = https.request(req)
+      # 返却の中身を見てみる
+      puts res.code
+      puts res.message
+      puts res.body
+    end
   end
+  rescue Timeout::Error
+    puts 'timeout error'
+  end
+
   def create
     @photo = Photo.new
      uploader = ImageUploader.new
@@ -34,7 +52,7 @@ class PhotosController < ApplicationController
       redirect_to action: :index
     end
   end
-  
+
   private
     def photos_params
       params.require(:photo).permit(:image)
